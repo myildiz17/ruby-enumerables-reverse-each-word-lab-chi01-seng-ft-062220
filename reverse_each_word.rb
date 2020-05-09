29352c6c3c0f88d7f3d85504a9f0a99d63898478
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-09 20:17:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -18,3 +18,23 @@ def reverse_each_word (string)
     end
     combined_reversed
 end
+
+
+def reverse_each_word (string)
+#reversed=[]
+#  string=string.split("")
+#  string.each do {|i| reversed.unshifted(i)}
+#    "selam kelam hacim"
+
+    string=string.split(" ")
+    count=0
+    reversed=[]
+    while count<string.size do
+      string.collect do |element|
+      element.reverse
+    count+=1
+    end
+    combined_reversed= string.join(" ")
+    end
+    combined_reversed
+end
