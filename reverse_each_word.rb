a3d34084261f5ae2687862701c262327b8aeeba4
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-09 20:37:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,40 +1,40 @@
 
 
-def reverse_each_word (string)
-#reversed=[]
-#  string=string.split("")
-#  string.each do {|i| reversed.unshifted(i)}
-#    "selam kelam hacim"
-
-    string=string.split(" ")
-    count=0
-    reversed=[]
-    while count<string.size do
-      string.each do |element|
-      reversed<<element.reverse
-    count+=1
-    end
-    combined_reversed= reversed.join(" ")
-    end
-    combined_reversed
-end
-
-
 #def reverse_each_word (string)
 #reversed=[]
 #  string=string.split("")
 #  string.each do {|i| reversed.unshifted(i)}
 #    "selam kelam hacim"
 
-    #string=string.split(" ")
-    #count=0
-    #reversed=[]
-    #while count<string.size do
-  #    string.collect do |element|
-#      element.reverse
+  #  string=string.split(" ")
+  #  count=0
+  #  reversed=[]
+  #  while count<string.size do
+  #    string.each do |element|
+  #    reversed<<element.reverse
   #  count+=1
   #  end
-  #  combined_reversed= string.join(" ")
+    combined_reversed= reversed.join(" ")
   #  end
   #  combined_reversed
 #end
+
+
+def reverse_each_word (string)
+ #reversed=[]
+  #string=string.split("")
+  #string.collect do {|i| reversed.unshifted(i)}
+    #{}"selam kelam hacim"
+
+    string=string.split(" ")
+    count=0
+    reversed=[]
+    while count<string.size do
+     string.collect do |element|
+     element.reverse
+    count+=1
+    end
+    combined_reversed= string.join(" ")
+    end
+    combined_reversed
+end
