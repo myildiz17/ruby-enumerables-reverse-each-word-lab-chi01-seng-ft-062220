470cd2483121cd4d0779443197c176a143732075
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-09 19:30:59 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,7 +1,11 @@
-def reverse(string)
- string = string.split(“”)
- reversed = []
- string.each {|i| reversed.unshift(i)}
- return reversed.join(“”)
-end
-reverse(“I like Ruby!”)
+#def reverse(string)
+ #string = string.split(“”)
+ #reversed = []
+ #string.each {|i| reversed.unshift(i)}
+ #return reversed.join(“”)
+#end
+#reverse(“I like Ruby!”)
+
+"selam kelam hacim"
+
+"selam kelam hacim".split("")
